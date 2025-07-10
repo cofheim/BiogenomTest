0ec47049bc1874966452d094a49fddeae3de5f85
cofheim <plastininbiz@yandex.ru>
2025-07-10 18:35:16 +0300

    спроектирована архитектура приложения

---
+++ BiogenomTest.Infrastructure/BiogenomTestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BiogenomTest.Infrastructure
+{
+    public class BiogenomTestDbContext : DbContext
+    {
+        public BiogenomTestDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
